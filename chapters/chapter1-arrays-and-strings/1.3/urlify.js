0ce158f20c1958e854c0818fdc25515fedbffdb8
OneTwoDan <danielondonoa@gmail.com>
2024-05-18 11:33:41 -0500

    exercise 1.3

--- chapters/chapter1-arrays-and-strings/1.3/urlify.js
+++ chapters/chapter1-arrays-and-strings/1.3/urlify.js
@@ -1,7 +1,43 @@
 // Write a method to replace all space in a string with ‘%20’. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the “true” length of the string.
 
-function urlify(str, trueLength) {
+// Example
+// input:  "Mr John Smith    " , 13
+// output: "Mr%20John%20Smith"
 
+// input:  "    " , 1
+// output: "%20"
+
+function urlify(str, trueLength) {
+    let trimmedStr = str.trim();
+
+    if (trimmedStr === '') {
+        return '%20'.repeat(trueLength);
+    }
+
+    let charArray = trimmedStr.split('');
+    let spaceCount = 0;
+
+    for (let i = 0; i < trueLength; i++) {
+        if (charArray[i] === ' ') {
+            spaceCount++;
+        }
+    }
+
+    let newLength = trueLength + spaceCount * 2;
+
+    for (let i = trueLength - 1; i >= 0; i--) {
+        if (charArray[i] === ' ') {
+            charArray[newLength - 1] = '0';
+            charArray[newLength - 2] = '2';
+            charArray[newLength - 3] = '%';
+            newLength -= 3;
+        } else {
+            charArray[newLength - 1] = charArray[i];
+            newLength -= 1;
+        }
+    }
+
+    return charArray.join('');
 }
 
-module.exports = urlify;
+module.exports = { urlify };
