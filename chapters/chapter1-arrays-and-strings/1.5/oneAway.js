98924a955fdb043b80f23fc064c2e3f06d8e8fde
OneTwoDan <danielondonoa@gmail.com>
2024-05-22 14:04:20 -0500

    solution exercise 1.5

--- chapters/chapter1-arrays-and-strings/1.5/oneAway.js
+++ chapters/chapter1-arrays-and-strings/1.5/oneAway.js
@@ -9,8 +9,37 @@ pales, pale -> true
 pale, bale -> true
 pale, bake -> false */
 
-function oneEditAway(str1, str2) {
+function oneEditAway(firstString, secondString) {
+    const length1 = firstString.length;
+    const length2 = secondString.length;
 
+    if (Math.abs(length1 - length2) > 1) {
+        return false;
+    }
+
+    const shorter = length1 < length2 ? firstString : secondString;
+    const longer = length1 < length2 ? secondString : firstString;
+
+    let indexShorter = 0, indexLonger = 0;
+    let foundDifference = false;
+
+    while (indexShorter < shorter.length && indexLonger < longer.length) {
+        if (shorter[indexShorter] !== longer[indexLonger]) {
+            if (foundDifference) {
+                return false;
+            }
+            foundDifference = true;
+
+            if (length1 === length2) {
+                indexShorter++;
+            }
+        } else {
+            indexShorter++;
+        }
+        indexLonger++;
+    }
+
+    return true;
 }
 
 module.exports = { oneEditAway }
