6ccd1e4cb0ba5d952c89b2b42e2f05019ff1f94c
OneTwoDan <danielondonoa@gmail.com>
2024-05-27 16:19:42 -0500

    tests for exercise 2.1

---
+++ chapters/chapter2-linked-lists/2.1/removeDups.test.js
@@ -0,0 +1,49 @@
+const { LinkedList } = require('./removeDups');
+
+describe('LinkedList', () => {
+    it('should remove duplicates from unsorted linked list', () => {
+        const list = new LinkedList();
+        list.append(3);
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(1);
+        list.append(4);
+
+        list.removeDuplicates();
+
+        expect(list.toArray()).toEqual([3, 1, 2, 4]);
+    });
+
+    it('should handle an empty list', () => {
+        const list = new LinkedList();
+
+        list.removeDuplicates();
+
+        expect(list.toArray()).toEqual([]);
+    });
+
+    it('should handle a list with no duplicates', () => {
+        const list = new LinkedList();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+
+        list.removeDuplicates();
+
+        expect(list.toArray()).toEqual([1, 2, 3, 4]);
+    });
+
+    it('should handle a list with all duplicates', () => {
+        const list = new LinkedList();
+        list.append(1);
+        list.append(1);
+        list.append(1);
+        list.append(1);
+
+        list.removeDuplicates();
+
+        expect(list.toArray()).toEqual([1]);
+    });
+});
