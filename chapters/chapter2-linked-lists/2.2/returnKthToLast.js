9ad2a6b88907b8db429b03259569ff349b1efccc
OneTwoDan <danielondonoa@gmail.com>
2024-05-28 17:55:56 -0500

    added exercise 2.2

--- chapters/chapter2-linked-lists/2.2/returnKthToLast.js
+++ chapters/chapter2-linked-lists/2.2/returnKthToLast.js
@@ -1,3 +1,43 @@
 // Implement an algorithm to find the kth to last element of a singly linked list.
 
+class ListNode {
+    constructor(value = 0, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
 
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        if (this.head === null) {
+            this.head = new ListNode(value);
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = new ListNode(value);
+    }
+
+    kthToLast(k) {
+        if (this.head === null || k < 0) return null;
+        let first = this.head;
+        let second = this.head;
+        for (let i = 0; i < k; i++) {
+            if (first === null) return null;
+            first = first.next;
+        }
+        while (first !== null && first.next !== null) {
+            first = first.next;
+            second = second.next;
+        }
+        return second.value;
+    }
+}
+
+module.exports = { LinkedList };
