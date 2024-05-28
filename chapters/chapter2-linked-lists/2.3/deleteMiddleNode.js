6d5a3124dcf8f07392e4b31f7a791fe58e0d4073
OneTwoDan <danielondonoa@gmail.com>
2024-05-28 18:00:14 -0500

    added solution exercise 2.3

--- chapters/chapter2-linked-lists/2.3/deleteMiddleNode.js
+++ chapters/chapter2-linked-lists/2.3/deleteMiddleNode.js
@@ -4,3 +4,23 @@ EXAMPLE
 lnput:the node c from the linked list a->b->c->d->e->f
 Result: nothing is returned, but the new linked list looks like a->b->d->e->f  */
 
+class ListNode {
+    constructor(value = 0, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+function deleteMiddleNode(node) {
+    if (node === null || node.next === null) {
+        return false;
+    }
+
+    node.value = node.next.value;
+
+    node.next = node.next.next;
+
+    return true;
+}
+
+module.exports = { ListNode, deleteMiddleNode };
