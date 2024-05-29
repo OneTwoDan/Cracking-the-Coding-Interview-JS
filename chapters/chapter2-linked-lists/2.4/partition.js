94ddd41a71b8b6c69d1e15b7abbaaa3b705cc034
OneTwoDan <danielondonoa@gmail.com>
2024-05-29 18:32:54 -0500

    added solution exercise 2.4

--- chapters/chapter2-linked-lists/2.4/partition.js
+++ chapters/chapter2-linked-lists/2.4/partition.js
@@ -4,3 +4,81 @@
 EXAMPLE
 Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition= 5]
 Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8 */
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    partition(x) {
+        if (!this.head || !this.head.next) return;
+
+        let beforeStart = null;
+        let beforeEnd = null;
+        let afterStart = null;
+        let afterEnd = null;
+
+        let current = this.head;
+        while (current) {
+            const next = current.next;
+            current.next = null;
+            if (current.data < x) {
+                if (!beforeStart) {
+                    beforeStart = current;
+                    beforeEnd = beforeStart;
+                } else {
+                    beforeEnd.next = current;
+                    beforeEnd = current;
+                }
+            } else {
+                if (!afterStart) {
+                    afterStart = current;
+                    afterEnd = afterStart;
+                } else {
+                    afterEnd.next = current;
+                    afterEnd = current;
+                }
+            }
+            current = next;
+        }
+
+        if (!beforeStart) {
+            this.head = afterStart;
+        } else {
+            beforeEnd.next = afterStart;
+            this.head = beforeStart;
+        }
+    }
+
+    toArray() {
+        const result = [];
+        let current = this.head;
+        while (current) {
+            result.push(current.data);
+            current = current.next;
+        }
+        return result;
+    }
+}
+
+module.exports = LinkedList;
